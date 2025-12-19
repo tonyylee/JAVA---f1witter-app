c4f3034532503d30b0a07aabe5b95b5387494d9a
tony lee <tonyylee4@gmail.com>
2025-12-18 20:55:25 -0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,57 @@
+export const tweetsData = [   
+    {
+        handle: `@maxverstappen1✔️ `,
+        profilePic: `images/sidverstappen.jpg`,
+        likes: 80000,
+        retweets: 10000,
+        tweetText: `won another race #simplylovely #las vegasgp25`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@FIA✔️`,
+        profilePic: `images/fia.png`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `say goodbye to DRS and hello to MOM`,
+        replies: [
+                  {
+                handle: `@landonorris✔️`,
+                profilePic: `images/landonorris.png`,
+                tweetText: `if Im on pole, i should be okay... right?`,
+            },
+                  {
+                handle: `@ferrarif1✔️`,
+                profilePic: `images/ferrari.jpg`,
+                tweetText: `WHYYYYY?!!??!?!?!`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@kimiantonelli✔️`,
+        profilePic: `images/kimiant.jpg`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `@maxverstappen1 can you adopt me pls`,
+        replies: [
+            {
+                handle: `@maxverstappen1✔️`,
+                profilePic: `images/sidverstappen.jpg`,
+                tweetText: `move to redbull then maybe`,
+            },
+            {
+                handle: `@yukitsunoda✔️`,
+                profilePic: `images/yukitsu.jpg`,
+                tweetText: `wait ...am i still gonna be on the team...?`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
